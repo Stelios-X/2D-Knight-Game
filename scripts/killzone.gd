2e26c9c8f405eb365e2177f86afd26a7219d41d6
Robert Angote <ebobby@outlook.com>
2024-07-31 11:19:15 +0300

    Dying 2.0
    Adding a game slow mo feature plus dying animation for player before reset.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,8 +5,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You died B!") 
+	Engine.time_scale = 0.5 #System will run at half the speed
+	body.get_node("CollisionShape2D").queue_free() #Removal of the node(queue_	free does that)
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0 #reset timescale to default
 	get_tree().reload_current_scene()
