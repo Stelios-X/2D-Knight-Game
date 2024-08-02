e3b0c8759fc05d2932524c8c0f4ab4e0165aa26b
Robert Angote <ebobby@outlook.com>
2024-08-02 10:10:38 +0300

    Score feature
    Adding the score and xp feature when you collect coins, a counter.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
+@onready var game_manager = %GameManager
 
 
 func _on_body_entered(body):
 	#pass # Replace with function body.
-	print("+1 new coin")
+	game_manager.add_point()
 	queue_free()
